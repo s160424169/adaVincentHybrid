50c61c6e465af01c0bb64d84738a6222eff81421
s160424169 <s160424169@student.ubaya.ac.id>
2026-09-17 22:56:30 +0700

    di keranjang.page,ts saya tambahkan beberapa method dan juga import dari service keranjang, pada htmlnya saya tambahkan ui button

--- src/app/keranjang/keranjang.page.ts
+++ src/app/keranjang/keranjang.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Keranjang } from '../services/keranjang';
+import { Transaksi } from '../services/transaksi';
 
 @Component({
   selector: 'app-keranjang',
@@ -7,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class KeranjangPage implements OnInit {
+  bump: number | null = null;
 
-  constructor() { }
+  constructor(public keranjang: Keranjang, private transaksi: Transaksi) { }
 
   ngOnInit() {
+  }
+  tambah(item: Keranjang['items'][number]) {
+    this.keranjang.tambahQty(item);
+    this.mainkanBump(item.produkId);
+  }
+
+  kurang(item: Keranjang['items'][number]) {
+    this.keranjang.kurangQty(item);
+    this.mainkanBump(item.produkId);
+  }
+
+  mainkanBump(id: number) {
+    this.bump = id;
+    setTimeout(() => { this.bump = null; }, 200);
+  }
+
+  hapus(item: Keranjang['items'][number], slidingItem: any) {
+    this.keranjang.hapus(item.produkId);
+    slidingItem.close();
+  }
+
+  konfirmasi() {
+    this.transaksi.konfirmasi(this.keranjang.items, this.keranjang.totalHarga);
+    this.keranjang.kosongkan();
   }
 
 }
